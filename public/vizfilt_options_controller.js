68622c9fe48c15c00f9244573fa0ba8afc425fda
Talal Shoeb <talalshoeb@Talals-MacBook-Pro.local>
2018-05-29 07:03:26 +0300

    enabled elasticsearch for options data fetching

--- public/vizfilt_options_controller.js
+++ public/vizfilt_options_controller.js
@@ -1,6 +1,6 @@
 import { uiModules } from 'ui/modules';
 
-const module = uiModules.get('kibana/vizfilt', ['kibana']);
+const module = uiModules.get('kibana/vizfilt', ['kibana'], ['elasticsearch']);
 
 module.service('client', function (esFactory) {
   return esFactory({
@@ -11,9 +11,30 @@ module.service('client', function (esFactory) {
 });
 
 module.controller('VizfiltOptionsController', ($scope, client, esFactory) => {
+	var indexlist = [];
+
 	client.cat.indices({
-		h: ['index', 'docs.count']
+		h: ['index']
 	}).then(function (body) {
-		console.log(body);
+		var index = body.split("\n");
+		var i;
+		for (i = 0; i < index.length; i++) { 
+		    if(index[i] != '.kibana' && index[i] != ''){
+		    	indexlist.push(index[i]);
+		    }
+		}
+		// console.log(indexlist);
+		$scope.idlist = indexlist;
+	});
+
+	client.indices.getMapping({
+		index: indexlist
+	}).then(function (body) {
+		for (var key in body) {
+			if(key != '.kibana'){
+				console.log(key);
+				console.log(body[key]['mappings']['requests']['properties']);
+			}
+		}
 	});
 });
